6a9adc2f876e81b4b2a190dee4a5dfe2553bf879
Golam Rabbani <golamrabbani9373@gmail.com>
2024-12-24 21:26:52 +0600

    install zod and add user profile form validatio

--- components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/MyProfileForm.jsx
+++ components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/MyProfileForm.jsx
@@ -2,35 +2,51 @@
 
 import ATJForm from '@/components/form/ATJForm';
 import ATJInput from '@/components/form/ATJInput';
-
+import {useGetMyProfile} from '@/hooks/users/users.hook';
+import {userProfileValidation} from '@/schemas/users/users.schema';
+import {zodResolver} from '@hookform/resolvers/zod';
+import {useSelector} from 'react-redux';
 const MyProfileForm = () => {
+	const user = useSelector((state) => state.user);
+
+	const {data, isPending} = useGetMyProfile(user.email);
+	const defaultValues = {
+		name: data?.name,
+		username: data?.username,
+		email: data?.email,
+		phone: data?.phone,
+	};
 	const handelProfileData = (data) => {
 		console.log('Profile Data', data);
 	};
 	return (
-		<ATJForm onSubmit={handelProfileData}>
+		<ATJForm
+			defaultValues={defaultValues}
+			resolver={zodResolver(userProfileValidation)}
+			onSubmit={handelProfileData}
+		>
 			<div className="default-form">
 				<div className="row">
 					{/* <!-- Input --> */}
 					<div className="form-group col-lg-6 col-md-12">
 						<label>Full Name</label>
-						<ATJInput type={'text'} label="Jerome Arnold" name="name" />
+						<ATJInput disabled={isPending} type={'text'} label="Jerome Arnold" name="name" />
 					</div>
 					{/* <!-- Input --> */}
 					<div className="form-group col-lg-6 col-md-12">
 						<label>User Name</label>
-						<ATJInput type={'text'} label="JeromeArnold" name="username" />
+						<ATJInput disabled={isPending} type={'text'} label="JeromeArnold" name="username" />
 					</div>
 					{/* <!-- Input --> */}
 					<div className="form-group col-lg-6 col-md-12">
 						<label>Email address</label>
 
-						<ATJInput type={'email'} label="jeromearnold@gmail.com" name="email" />
+						<ATJInput disabled={true} type={'email'} label="jeromearnold@gmail.com" name="email" />
 					</div>
 					{/* <!-- Input --> */}
 					<div className="form-group col-lg-6 col-md-12">
 						<label>Phone</label>
-						<ATJInput type={'text'} label="0 123 456 7890" name="phone" />
+						<ATJInput disabled={isPending} type={'text'} label="0 123 456 7890" name="phone" />
 					</div>
 					{/* <!-- Input --> */}
 					<div className="form-group col-lg-6 col-md-12">

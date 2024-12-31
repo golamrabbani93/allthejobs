cdd86828bdf89b3d028c9c3ded20845b1cee8eb6
Golam Rabbani <golamrabbani9373@gmail.com>
2024-12-31 16:38:27 +0600

    all role Profile Complete

--- components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/MyProfile.jsx
+++ components/dashboard-pages/candidates-dashboard/my-profile/components/my-profile/MyProfile.jsx
@@ -4,6 +4,7 @@ import MyProfileForm from './MyProfileForm';
 const MyProfile = () => {
 	return (
 		<div className="widget-content">
+			<label className="font-semibold mb-2">Upload Your Photo</label>
 			<LogoUpload />
 			{/* End logo and cover photo components */}
 

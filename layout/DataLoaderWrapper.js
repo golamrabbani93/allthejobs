f94c53b3015cfee37bbc2b94c09f976c26b41032
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-05 16:05:52 +0600

    get jobs data in find jobs page

--- layout/DataLoaderWrapper.js
+++ layout/DataLoaderWrapper.js
@@ -9,29 +9,29 @@ import {useEffect, useState} from 'react';
 import {useDispatch} from 'react-redux';
 
 const DataLoaderWrapper = ({children}) => {
-	const [loading, setLoading] = useState(true);
 	const dispatch = useDispatch();
 	//load all data
 	const fetchAllData = async () => {
 		try {
-			dispatch(setGlobalDataLoading(true));
 			const data = await fetchData();
 			dispatch(setTalentsData(data.talents));
 			dispatch(setJobsData(data.jobs));
 			dispatch(setConsultantsData(data.consultants));
+			dispatch(setGlobalDataLoading(false));
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		} finally {
 			dispatch(setGlobalDataLoading(false));
-			setLoading(false);
 		}
 	};
 	useEffect(() => {
 		// fetch all data and revalidate every 8 minutes
+		dispatch(setGlobalDataLoading(true));
 		fetchAllData();
 		const interval = setInterval(() => {
+			dispatch(setGlobalDataLoading(false));
 			fetchAllData();
-		}, 480000);
+		}, 48000);
 		return () => clearInterval(interval);
 	}, []);
 	return <>{children}</>;

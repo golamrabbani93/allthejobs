b88031986c425ee55d9d948f1954a0e37be6a4f2
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-06 16:47:02 +0600

    consultant experience details complete

--- components/dashboard-pages/consultant-dashboard/company-profile/components/my-profile/ExperienceDetails.jsx
+++ components/dashboard-pages/consultant-dashboard/company-profile/components/my-profile/ExperienceDetails.jsx
@@ -2,7 +2,7 @@
 import React, {useEffect, useState} from 'react';
 import ATJForm from '@/components/form/ATJForm';
 import ATJInput from '@/components/form/ATJInput';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {
 	useGetTalentQuery,
 	useUpdateTalentMutation,
@@ -10,26 +10,20 @@ import {
 import Spinner from '@/components/Sppiner/Spinner';
 import {zodResolver} from '@hookform/resolvers/zod';
 import {experienceValidationSchema} from '@/schemas/experience.schema';
-import {
-	useGetConsultantQuery,
-	useUpdateConsultantMutation,
-} from '@/features/consultant/consultant.management.api';
+import {useUpdateConsultantMutation} from '@/features/consultant/consultant.management.api';
+import {setUserRoleBasedData} from '@/features/data/dataSlice';
 
 const ExperienceDetails = () => {
+	const dispatch = useDispatch();
 	//get user
-	const user = useSelector((state) => state.user);
-
-	//get talent profile
-
-	const {data: consultantData, isFetching} = useGetConsultantQuery(user.user_id);
+	const {userRoleBasedData, loading} = useSelector((state) => state.data);
 	//update education details
-
 	const [updateConsultant, {data, isLoading}] = useUpdateConsultantMutation();
 	const [showForm, setShowForm] = useState(false); // State to control form visibility
 
 	const handelProfileData = (data, e) => {
 		const payload = {
-			id: consultantData.experience_details.length + 1,
+			id: userRoleBasedData.experience_details.length + 1,
 			role: data.role,
 			companyName: data.companyName,
 			duration: data.duration,
@@ -37,13 +31,13 @@ const ExperienceDetails = () => {
 		};
 		const newData = {
 			experience_details:
-				consultantData.experience_details.length > 0
-					? [...consultantData.experience_details, payload]
+				userRoleBasedData.experience_details.length > 0
+					? [...userRoleBasedData.experience_details, payload]
 					: [payload],
-			user_id: consultantData.user_id,
-			headline: consultantData.headline,
+			user_id: userRoleBasedData.user_id,
+			headline: userRoleBasedData.headline,
 		};
-		updateConsultant({consultantId: consultantData.consultant_id, data: newData});
+		updateConsultant({consultantId: userRoleBasedData.consultant_id, data: newData});
 
 		e.target.reset(); // Reset the form
 	};
@@ -51,6 +45,7 @@ const ExperienceDetails = () => {
 	//handle close form
 	useEffect(() => {
 		if (data?.consultant_id) {
+			dispatch(setUserRoleBasedData(data));
 			setShowForm(false);
 		}
 	}, [data]);
@@ -70,7 +65,7 @@ const ExperienceDetails = () => {
 					</button>
 				) : (
 					<button
-						disabled={!consultantData?.consultant_id || isFetching || isLoading}
+						disabled={!userRoleBasedData?.consultant_id || loading || isLoading}
 						className="theme-btn btn-style-one mb-4"
 						onClick={handleAddNewEducation}
 					>
@@ -87,17 +82,12 @@ const ExperienceDetails = () => {
 							<div className="row">
 								<div className="form-group col-lg-6 col-md-12">
 									<label>Role</label>
-									<ATJInput
-										disabled={isFetching}
-										type={'text'}
-										label="Sr UX Engineer"
-										name="role"
-									/>
+									<ATJInput disabled={loading} type={'text'} label="Sr UX Engineer" name="role" />
 								</div>
 								<div className="form-group col-lg-6 col-md-12">
 									<label>Company Name Name</label>
 									<ATJInput
-										disabled={isFetching}
+										disabled={loading}
 										type={'text'}
 										label="Spotify Inc."
 										name="companyName"
@@ -105,17 +95,12 @@ const ExperienceDetails = () => {
 								</div>
 								<div className="form-group col-lg-6 col-md-12">
 									<label>Duration</label>
-									<ATJInput
-										disabled={isFetching}
-										type={'text'}
-										label="2012 - 2014"
-										name="duration"
-									/>
+									<ATJInput disabled={loading} type={'text'} label="2012 - 2014" name="duration" />
 								</div>
 								<div className="form-group col-lg-6 col-md-12">
 									<label>Description</label>
 									<ATJInput
-										disabled={isFetching}
+										disabled={loading}
 										type={'text'}
 										label="Tell us about your role"
 										name="description"
@@ -135,7 +120,7 @@ const ExperienceDetails = () => {
 				{/* Display saved education details */}
 				<div className={`resume-outer theme-blue`}>
 					{/* <!-- Start Resume BLock --> */}
-					{consultantData?.experience_details?.map((item, i) => (
+					{userRoleBasedData?.experience_details?.map((item, i) => (
 						<div className="resume-block" key={i}>
 							<div className="inner">
 								<span className="name">{item.companyName.slice(0, 1)}</span>

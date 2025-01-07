864615aee6830e2f676625bd0a7dab73c43024d0
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-07 16:47:46 +0600

    home page consultation slider complete section slider complete

---
+++ components/consultants/ConsultantsSlider.jsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Slider from 'react-slick';
+import Image from 'next/image';
+const ConsultantsSlider = () => {
+	const images = [
+		{src: '/images/consultants/consultant-1.png', alt: 'consultant-1'},
+		{src: '/images/consultants/consultant-2.png', alt: 'consultant-2'},
+		{src: '/images/consultants/consultant-3.png', alt: 'consultant-3'},
+		{src: '/images/consultants/consultant-4.png', alt: 'consultant-4'},
+		{src: '/images/consultants/consultant-5.png', alt: 'consultant-5'},
+		{src: '/images/consultants/consultant-6.png', alt: 'consultant-6'},
+		{src: '/images/consultants/consultant-7.png', alt: 'consultant-7'},
+		{src: '/images/consultants/consultant-8.png', alt: 'consultant-8'},
+		{src: '/images/consultants/consultant-9.png', alt: 'consultant-9'},
+	];
+
+	// Settings
+	const settings = {
+		dots: false,
+		speed: 1200,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		cssEase: 'linear',
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		pauseOnDotsHover: false,
+		fade: true,
+		autoplay: true,
+		autoplaySpeed: 3000,
+	};
+
+	return (
+		<>
+			<Slider {...settings} arrows={false}>
+				{images.map((img, i) => (
+					<Image
+						src={img.src} // Replace with your image path
+						alt={img.alt}
+						width={700}
+						height={400}
+						className="img-fluid"
+					/>
+				))}
+			</Slider>
+		</>
+	);
+};
+
+export default ConsultantsSlider;

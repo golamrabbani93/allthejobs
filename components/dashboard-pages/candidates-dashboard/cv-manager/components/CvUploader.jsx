4f633488b2b6c97595b394aa0df0786ec8ddfede
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-13 16:26:43 +0600

    cv upload complete

--- components/dashboard-pages/candidates-dashboard/cv-manager/components/CvUploader.jsx
+++ components/dashboard-pages/candidates-dashboard/cv-manager/components/CvUploader.jsx
@@ -2,9 +2,10 @@
 
 import Spinner from '@/components/Sppiner/Spinner';
 import {useUpdateTalentMutation} from '@/features/candidate/talent.management.api';
-import {setTalentsData} from '@/features/data/dataSlice';
+import {setGlobalDataLoading, setTalentsData} from '@/features/data/dataSlice';
 import {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {string} from 'zod';
 
 // validation chaching
 function checkFileTypes(files) {
@@ -23,10 +24,9 @@ function checkFileTypes(files) {
 
 const CvUploader = () => {
 	// get user role based data
-	const {userRoleBasedData, Loading} = useSelector((state) => state.data);
-	console.log('🚀🚀: CvUploader -> userRoleBasedData', userRoleBasedData);
+	const {userRoleBasedData, loading} = useSelector((state) => state.data);
+
 	const [updateTalentCv, {data, isLoading}] = useUpdateTalentMutation();
-	console.log('🚀🚀: CvUploader -> data', data);
 	const [getManager, setManager] = useState([]);
 	const [getError, setError] = useState('');
 	const dispatch = useDispatch();
@@ -64,8 +64,13 @@ const CvUploader = () => {
 	useEffect(() => {
 		if (data?.talent_id) {
 			dispatch(setTalentsData(data));
+			dispatch(setGlobalDataLoading(true));
+			setManager([]);
 		}
-	}, [data]);
+		if (userRoleBasedData?.resume) {
+			dispatch(setGlobalDataLoading(false));
+		}
+	}, [data, userRoleBasedData]);
 
 	// delete image
 	const deleteHandler = (name) => {
@@ -75,55 +80,75 @@ const CvUploader = () => {
 
 	return (
 		<>
-			{/* Start Upload resule */}
-			<div className="uploading-resume">
-				<div className="uploadButton">
-					<input
-						className="uploadButton-input"
-						type="file"
-						name="attachments[]"
-						accept=".doc,.docx,.xml,application/msword,application/pdf, image/*"
-						id="upload"
-						multiple
-						onChange={cvManagerHandler}
-					/>
-					<label className="cv-uploadButton" htmlFor="upload">
-						<span className="title">Drop files here to upload</span>
-						<span className="text">
-							To upload file size is (Max 5Mb) and allowed file types are (.doc, .docx, .pdf)
-						</span>
-
-						<span className="theme-btn btn-style-one">Select Resume</span>
-						{getError !== '' ? <p className="ui-danger mb-0">{getError}</p> : undefined}
-					</label>
-					<span className="uploadButton-file-name"></span>
+			{loading ? (
+				<div className="h-96 flex justify-center items-center">
+					<Spinner size="md" color="primary" />
 				</div>
-			</div>
-			{/* End upload-resume */}
-
-			{/* Start resume Preview  */}
-			<div className="files-outer items-center">
-				{getManager.length > 0 &&
-					getManager?.map((file, i) => (
-						<div key={i} className="file-edit-box">
-							<span className="title">{file.name}</span>
-							<div className="edit-btns">
-								<button>
-									<span className="la la-pencil"></span>
-								</button>
-								<button onClick={() => deleteHandler(file.name)}>
-									<span className="la la-trash"></span>
-								</button>
+			) : (
+				<>
+					{userRoleBasedData?.resume && (
+						<div className={`mb-4 `}>
+							<h4 className="capitalize mb-4">You already Upload Your CV </h4>
+							<div>
+								<a href={userRoleBasedData?.resume} target="_blank" rel="noopener noreferrer">
+									<button className="theme-btn btn-style-one h-[40px]">Open CV</button>
+								</a>
 							</div>
 						</div>
-					))}
-				{getManager.length > 0 && (
-					<button onClick={() => handleCvUpload()} className="theme-btn btn-style-one h-[40px]">
-						{isLoading ? <Spinner size="sm" color="white" /> : 'Upload Resume'}
-					</button>
-				)}
-			</div>
-			{/* End resume Preview  */}
+					)}
+					{/* Start Upload resume */}
+
+					<div className="uploading-resume">
+						<div className="uploadButton">
+							<input
+								className="uploadButton-input"
+								type="file"
+								name="attachments[]"
+								accept=".doc,.docx,.xml,application/msword,application/pdf, image/*"
+								id="upload"
+								multiple
+								onChange={cvManagerHandler}
+							/>
+							<label className="cv-uploadButton" htmlFor="upload">
+								<span className="title">Drop files here to upload</span>
+								<span className="text">
+									To upload file size is (Max 5Mb) and allowed file types are (.doc, .docx, .pdf)
+								</span>
+
+								<span className="theme-btn btn-style-one">Select Resume</span>
+								{getError !== '' ? <p className="ui-danger mb-0">{getError}</p> : undefined}
+							</label>
+							<span className="uploadButton-file-name"></span>
+						</div>
+					</div>
+
+					{/* End upload-resume */}
+
+					{/* Start resume Preview  */}
+					<div className="files-outer items-center">
+						{getManager.length > 0 &&
+							getManager?.map((file, i) => (
+								<div key={i} className="file-edit-box">
+									<span className="title">{file.name}</span>
+									<div className="edit-btns">
+										<button>
+											<span className="la la-pencil"></span>
+										</button>
+										<button onClick={() => deleteHandler(file.name)}>
+											<span className="la la-trash"></span>
+										</button>
+									</div>
+								</div>
+							))}
+						{getManager.length > 0 && (
+							<button onClick={() => handleCvUpload()} className="theme-btn btn-style-one h-[40px]">
+								{isLoading ? <Spinner size="sm" color="white" /> : 'Upload Resume'}
+							</button>
+						)}
+					</div>
+					{/* End resume Preview  */}
+				</>
+			)}
 		</>
 	);
 };

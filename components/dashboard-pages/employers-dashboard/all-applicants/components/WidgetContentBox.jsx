c4e20f77268b1cfb1ba5fed4a06137f3ce688ff8
Golam Rabbani <golamrabbani9373@gmail.com>
2025-01-14 17:28:18 +0600

    make part by part component fot talents show

--- components/dashboard-pages/employers-dashboard/all-applicants/components/WidgetContentBox.jsx
+++ components/dashboard-pages/employers-dashboard/all-applicants/components/WidgetContentBox.jsx
@@ -1,22 +1,17 @@
 'use client';
 
 import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
-import Link from 'next/link';
-import Image from 'next/image';
-import {
-	useGetAllApplicationsQuery,
-	useUpdateApplicationStatusMutation,
-} from '@/features/application/application.management.api';
+
+import {useGetAllApplicationsQuery} from '@/features/application/application.management.api';
 import {useGetAllTalentsQuery} from '@/features/candidate/talent.management.api';
 import Spinner from '@/components/Sppiner/Spinner';
-import {useRouter} from 'next/navigation';
+import AllTalents from './talents/AllTalents';
+import ShortListTalents from './talents/ShortListTalents';
+import InterviewingTalents from './talents/InterviewingTalents';
+import HiredTalents from './talents/HiredTalents';
+import RejectedTalents from './talents/RejectedTalents';
 
 const WidgetContentBox = ({selectJob}) => {
-	const router = useRouter();
-	//update applicant status
-	const [updateApplicantStatus, {data, isLoading: updateAppLoading}] =
-		useUpdateApplicationStatusMutation();
-	console.log('🚀🚀: WidgetContentBox -> data', data);
 	//get all talents
 	const {data: talents, isLoading: talentsLoading} = useGetAllTalentsQuery();
 	//get all applicants
@@ -54,20 +49,6 @@ const WidgetContentBox = ({selectJob}) => {
 		talents.find((talent) => talent.talent_id === applicant.talent_id),
 	);
 
-	//handel make shortlist
-	const handleMakeShortList = (candidate) => {
-		const getApplication = applicants.find(
-			(applicant) => applicant.talent_id === candidate.talent_id,
-		);
-		const payload = {
-			job_application_id: getApplication?.job_application_id,
-			job_id: getApplication?.job_id,
-			status: 'short-listed',
-			talent_id: getApplication?.talent_id,
-		};
-
-		updateApplicantStatus(payload);
-	};
 	if (talentsLoading || applicantLoading)
 		return (
 			<div className="flex justify-center items-center h-96">
@@ -103,87 +84,12 @@ const WidgetContentBox = ({selectJob}) => {
 							<div className="row">
 								{allAppliedTalents && allAppliedTalents?.length > 0 ? (
 									allAppliedTalents?.map((candidate) => (
-										<div
-											className="candidate-block-three col-lg-6 col-md-12 col-sm-12"
+										<AllTalents
 											key={candidate?.talent_id}
-										>
-											<div className="inner-box">
-												<div className="content">
-													<figure className="image">
-														{candidate?.user?.photo && (
-															<Image
-																width={90}
-																height={90}
-																src={candidate.user.photo}
-																alt="candidates"
-															/>
-														)}
-													</figure>
-													<h4 className="name">
-														<Link href={`/candidates-single-v1/${candidate?.talent_id}`}>
-															{candidate?.user?.name}
-														</Link>
-													</h4>
-
-													<ul className="candidate-info">
-														<li className="designation">{candidate?.headline}</li>
-														<li>
-															<span className="icon flaticon-map-locator"></span>{' '}
-															{candidate?.country}
-														</li>
-														<li>
-															<span className="icon flaticon-money"></span> $
-															{candidate?.expected_salary}
-														</li>
-													</ul>
-													{/* End candidate?-info */}
-
-													<ul className="post-tags">
-														{candidate?.skills?.map((val, i) => (
-															<li key={i}>
-																<a href="#">{val}</a>
-															</li>
-														))}
-													</ul>
-												</div>
-												{/* End content */}
-
-												{selectJob?.job_id && (
-													<div className="option-box">
-														<ul className="option-list">
-															<li>
-																<button
-																	onClick={() => router.push(`/talents/${candidate?.talent_id}`)}
-																	data-text="View Talent"
-																>
-																	<span className="la la-eye"></span>
-																</button>
-															</li>
-															<li>
-																{updateAppLoading ? (
-																	<button data-text="Loading...">
-																		<span className="la la-spinner la-spin"></span>
-																	</button>
-																) : (
-																	<button
-																		onClick={() => handleMakeShortList(candidate)}
-																		data-text="Make Shortlist"
-																	>
-																		<span className="la la-check"></span>
-																	</button>
-																)}
-															</li>
-															<li>
-																<button data-text="Reject Application">
-																	<span className="la la-times-circle"></span>
-																</button>
-															</li>
-														</ul>
-													</div>
-												)}
-												{/* End admin options box */}
-											</div>
-										</div>
+											candidate={candidate}
+											applicants={applicants}
+											selectJob={selectJob}
+										/>
 									))
 								) : (
 									<div className="flex justify-center items-center h-40">
@@ -199,78 +105,12 @@ const WidgetContentBox = ({selectJob}) => {
 									<div className="row">
 										{shortListedTalents?.length > 0 ? (
 											shortListedTalents?.map((candidate) => (
-												<div
-													className="candidate-block-three col-lg-6 col-md-12 col-sm-12"
+												<ShortListTalents
 													key={candidate?.talent_id}
-												>
-													<div className="inner-box">
-														<div className="content">
-															<figure className="image">
-																{candidate?.user?.photo && (
-																	<Image
-																		width={90}
-																		height={90}
-																		src={candidate.user.photo}
-																		alt="candidates"
-																	/>
-																)}
-															</figure>
-															<h4 className="name">
-																<Link href={`/candidates-single-v1/${candidate?.talent_id}`}>
-																	{candidate?.user?.name}
-																</Link>
-															</h4>
-
-															<ul className="candidate-info">
-																<li className="designation">{candidate?.headline}</li>
-																<li>
-																	<span className="icon flaticon-map-locator"></span>{' '}
-																	{candidate?.country}
-																</li>
-																<li>
-																	<span className="icon flaticon-money"></span> $
-																	{candidate?.expected_salary}
-																</li>
-															</ul>
-															{/* End candidate?-info */}
-
-															<ul className="post-tags">
-																{candidate?.skills?.map((val, i) => (
-																	<li key={i}>
-																		<a href="#">{val}</a>
-																	</li>
-																))}
-															</ul>
-														</div>
-														{/* End content */}
-
-														<div className="option-box">
-															<ul className="option-list">
-																<li>
-																	<button data-text="View Aplication">
-																		<span className="la la-eye"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Approve Aplication">
-																		<span className="la la-check"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Reject Aplication">
-																		<span className="la la-times-circle"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Delete Aplication">
-																		<span className="la la-trash"></span>
-																	</button>
-																</li>
-															</ul>
-														</div>
-														{/* End admin options box */}
-													</div>
-												</div>
+													candidate={candidate}
+													applicants={applicants}
+													selectJob={selectJob}
+												/>
 											))
 										) : (
 											<div className="flex justify-center items-center h-40">
@@ -284,78 +124,12 @@ const WidgetContentBox = ({selectJob}) => {
 									<div className="row">
 										{interviewListedTalents?.length > 0 ? (
 											interviewListedTalents?.map((candidate) => (
-												<div
-													className="candidate-block-three col-lg-6 col-md-12 col-sm-12"
+												<InterviewingTalents
 													key={candidate?.talent_id}
-												>
-													<div className="inner-box">
-														<div className="content">
-															<figure className="image">
-																{candidate?.user?.photo && (
-																	<Image
-																		width={90}
-																		height={90}
-																		src={candidate.user.photo}
-																		alt="candidates"
-																	/>
-																)}
-															</figure>
-															<h4 className="name">
-																<Link href={`/candidates-single-v1/${candidate?.talent_id}`}>
-																	{candidate?.user?.name}
-																</Link>
-															</h4>
-
-															<ul className="candidate-info">
-																<li className="designation">{candidate?.headline}</li>
-																<li>
-																	<span className="icon flaticon-map-locator"></span>{' '}
-																	{candidate?.country}
-																</li>
-																<li>
-																	<span className="icon flaticon-money"></span> $
-																	{candidate?.expected_salary}
-																</li>
-															</ul>
-															{/* End candidate?-info */}
-
-															<ul className="post-tags">
-																{candidate?.skills?.map((val, i) => (
-																	<li key={i}>
-																		<a href="#">{val}</a>
-																	</li>
-																))}
-															</ul>
-														</div>
-														{/* End content */}
-
-														<div className="option-box">
-															<ul className="option-list">
-																<li>
-																	<button data-text="View Aplication">
-																		<span className="la la-eye"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Approve Aplication">
-																		<span className="la la-check"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Reject Aplication">
-																		<span className="la la-times-circle"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Delete Aplication">
-																		<span className="la la-trash"></span>
-																	</button>
-																</li>
-															</ul>
-														</div>
-														{/* End admin options box */}
-													</div>
-												</div>
+													candidate={candidate}
+													applicants={applicants}
+													selectJob={selectJob}
+												/>
 											))
 										) : (
 											<div className="flex justify-center items-center h-40">
@@ -369,78 +143,12 @@ const WidgetContentBox = ({selectJob}) => {
 									<div className="row">
 										{hiredTalents?.length > 0 ? (
 											hiredTalents?.map((candidate) => (
-												<div
-													className="candidate-block-three col-lg-6 col-md-12 col-sm-12"
+												<HiredTalents
 													key={candidate?.talent_id}
-												>
-													<div className="inner-box">
-														<div className="content">
-															<figure className="image">
-																{candidate?.user?.photo && (
-																	<Image
-																		width={90}
-																		height={90}
-																		src={candidate.user.photo}
-																		alt="candidates"
-																	/>
-																)}
-															</figure>
-															<h4 className="name">
-																<Link href={`/candidates-single-v1/${candidate?.talent_id}`}>
-																	{candidate?.user?.name}
-																</Link>
-															</h4>
-
-															<ul className="candidate-info">
-																<li className="designation">{candidate?.headline}</li>
-																<li>
-																	<span className="icon flaticon-map-locator"></span>{' '}
-																	{candidate?.country}
-																</li>
-																<li>
-																	<span className="icon flaticon-money"></span> $
-																	{candidate?.expected_salary}
-																</li>
-															</ul>
-															{/* End candidate?-info */}
-
-															<ul className="post-tags">
-																{candidate?.skills?.map((val, i) => (
-																	<li key={i}>
-																		<a href="#">{val}</a>
-																	</li>
-																))}
-															</ul>
-														</div>
-														{/* End content */}
-
-														<div className="option-box">
-															<ul className="option-list">
-																<li>
-																	<button data-text="View Aplication">
-																		<span className="la la-eye"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Approve Aplication">
-																		<span className="la la-check"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Reject Aplication">
-																		<span className="la la-times-circle"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Delete Aplication">
-																		<span className="la la-trash"></span>
-																	</button>
-																</li>
-															</ul>
-														</div>
-														{/* End admin options box */}
-													</div>
-												</div>
+													candidate={candidate}
+													applicants={applicants}
+													selectJob={selectJob}
+												/>
 											))
 										) : (
 											<div className="flex justify-center items-center h-40">
@@ -454,78 +162,12 @@ const WidgetContentBox = ({selectJob}) => {
 									<div className="row">
 										{rejectedTalents?.length > 0 ? (
 											rejectedTalents?.map((candidate) => (
-												<div
-													className="candidate-block-three col-lg-6 col-md-12 col-sm-12"
+												<RejectedTalents
 													key={candidate?.talent_id}
-												>
-													<div className="inner-box">
-														<div className="content">
-															<figure className="image">
-																{candidate?.user?.photo && (
-																	<Image
-																		width={90}
-																		height={90}
-																		src={candidate.user.photo}
-																		alt="candidates"
-																	/>
-																)}
-															</figure>
-															<h4 className="name">
-																<Link href={`/candidates-single-v1/${candidate?.talent_id}`}>
-																	{candidate?.user?.name}
-																</Link>
-															</h4>
-
-															<ul className="candidate-info">
-																<li className="designation">{candidate?.headline}</li>
-																<li>
-																	<span className="icon flaticon-map-locator"></span>{' '}
-																	{candidate?.country}
-																</li>
-																<li>
-																	<span className="icon flaticon-money"></span> $
-																	{candidate?.expected_salary}
-																</li>
-															</ul>
-															{/* End candidate?-info */}
-
-															<ul className="post-tags">
-																{candidate?.skills?.map((val, i) => (
-																	<li key={i}>
-																		<a href="#">{val}</a>
-																	</li>
-																))}
-															</ul>
-														</div>
-														{/* End content */}
-
-														<div className="option-box">
-															<ul className="option-list">
-																<li>
-																	<button data-text="View Aplication">
-																		<span className="la la-eye"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Approve Aplication">
-																		<span className="la la-check"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Reject Aplication">
-																		<span className="la la-times-circle"></span>
-																	</button>
-																</li>
-																<li>
-																	<button data-text="Delete Aplication">
-																		<span className="la la-trash"></span>
-																	</button>
-																</li>
-															</ul>
-														</div>
-														{/* End admin options box */}
-													</div>
-												</div>
+													candidate={candidate}
+													applicants={applicants}
+													selectJob={selectJob}
+												/>
 											))
 										) : (
 											<div className="flex justify-center items-center h-40">
---
+++ components/dashboard-pages/employers-dashboard/all-applicants/components/talents/HiredTalents.jsx
@@ -0,0 +1,95 @@
+import {useUpdateApplicationStatusMutation} from '@/features/application/application.management.api';
+import Image from 'next/image';
+import Link from 'next/link';
+import {useRouter} from 'next/navigation';
+
+const HiredTalents = ({candidate, applicants, selectJob}) => {
+	const router = useRouter();
+	//update applicant status
+	const [updateApplicantStatus, {data, isLoading: updateAppLoading}] =
+		useUpdateApplicationStatusMutation();
+	//handel make shortlist
+	const handleMakeShortList = (candidate) => {
+		const getApplication = applicants.find(
+			(applicant) => applicant.talent_id === candidate.talent_id,
+		);
+		const payload = {
+			job_application_id: getApplication?.job_application_id,
+			job_id: getApplication?.job_id,
+			status: 'short-listed',
+			talent_id: getApplication?.talent_id,
+		};
+
+		updateApplicantStatus(payload);
+	};
+	return (
+		<div className="candidate-block-three col-lg-6 col-md-12 col-sm-12">
+			<div className="inner-box">
+				<div className="content">
+					<figure className="image">
+						{candidate?.user?.photo && (
+							<Image width={90} height={90} src={candidate.user.photo} alt="candidates" />
+						)}
+					</figure>
+					<h4 className="name">
+						<Link href={`/talents/${candidate?.talent_id}`}>{candidate?.user?.name}</Link>
+					</h4>
+
+					<ul className="candidate-info">
+						<li className="designation">{candidate?.headline}</li>
+						<li>
+							<span className="icon flaticon-map-locator"></span> {candidate?.country}
+						</li>
+						<li>
+							<span className="icon flaticon-money"></span> ${candidate?.expected_salary}
+						</li>
+					</ul>
+					{/* End candidate?-info */}
+
+					<ul className="post-tags">
+						{candidate?.skills?.map((val, i) => (
+							<li key={i}>
+								<a href="#">{val}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+				{/* End content */}
+
+				{selectJob?.job_id && (
+					<div className="option-box">
+						<ul className="option-list">
+							<li>
+								<button
+									onClick={() => router.push(`/talents/${candidate?.talent_id}`)}
+									data-text="View Talent"
+								>
+									<span className="la la-eye"></span>
+								</button>
+							</li>
+							<li>
+								{updateAppLoading ? (
+									<button data-text="Loading...">
+										<span className="la la-spinner la-spin"></span>
+									</button>
+								) : (
+									<button onClick={() => handleMakeShortList(candidate)} data-text="Make Shortlist">
+										<span className="la la-check"></span>
+									</button>
+								)}
+							</li>
+							<li>
+								<button data-text="Reject Application">
+									<span className="la la-times-circle"></span>
+								</button>
+							</li>
+						</ul>
+					</div>
+				)}
+				{/* End admin options box */}
+			</div>
+		</div>
+	);
+};
+
+export default HiredTalents;

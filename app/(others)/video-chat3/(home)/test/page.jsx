9314a181f9d2c5ddacd878f70ec9bfeae7c7f1cc
Hamim <shoeb.hamim@gmail.com>
2025-01-18 19:16:18 +0600

    before adding dynamic slot options

--- app/(others)/video-chat3/(home)/test/page.jsx
+++ app/(others)/video-chat3/(home)/test/page.jsx
@@ -1,61 +1,97 @@
 "use client";
 import "react-datepicker/dist/react-datepicker.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
+import axios from "axios";
 const timeSlots = [
-  "12:00am-12:30am",
-  "12:30am-01:00am",
-  "01:00am-01:30am",
-  "01:30am-02:00am",
-  "02:00am-02:30am",
-  "02:30am-03:00am",
-  "03:00am-03:30am",
-  "03:30am-04:00am",
-  "04:00am-04:30am",
-  "04:30am-05:00am",
-  "05:00am-05:30am",
-  "05:30am-06:00am",
-  "06:00am-06:30am",
-  "06:30am-07:00am",
-  "07:00am-07:30am",
-  "07:30am-08:00am",
-  "08:00am-08:30am",
-  "08:30am-09:00am",
-  "09:00am-09:30am",
-  "09:30am-10:00am",
-  "10:00am-10:30am",
-  "10:30am-11:00am",
-  "11:00am-11:30am",
-  "11:30am-12:00pm",
-  "12:00pm-12:30pm",
-  "12:30pm-01:00pm",
-  "01:00pm-01:30pm",
-  "01:30pm-02:00pm",
-  "02:00pm-02:30pm",
-  "02:30pm-03:00pm",
-  "03:00pm-03:30pm",
-  "03:30pm-04:00pm",
-  "04:00pm-04:30pm",
-  "04:30pm-05:00pm",
-  "05:00pm-05:30pm",
-  "05:30pm-06:00pm",
-  "06:00pm-06:30pm",
-  "06:30pm-07:00pm",
-  "07:00pm-07:30pm",
-  "07:30pm-08:00pm",
-  "08:00pm-08:30pm",
-  "08:30pm-09:00pm",
-  "09:00pm-09:30pm",
-  "09:30pm-10:00pm",
-  "10:00pm-10:30pm",
-  "10:30pm-11:00pm",
-  "11:00pm-11:30pm",
-  "11:30pm-12:00am",
+  "12:00AM-12:30AM",
+  "12:30AM-01:00AM",
+  "01:00AM-01:30AM",
+  "01:30AM-02:00AM",
+  "02:00AM-02:30AM",
+  "02:30AM-03:00AM",
+  "03:00AM-03:30AM",
+  "03:30AM-04:00AM",
+  "04:00AM-04:30AM",
+  "04:30AM-05:00AM",
+  "05:00AM-05:30AM",
+  "05:30AM-06:00AM",
+  "06:00AM-06:30AM",
+  "06:30AM-07:00AM",
+  "07:00AM-07:30AM",
+  "07:30AM-08:00AM",
+  "08:00AM-08:30AM",
+  "08:30AM-09:00AM",
+  "09:00AM-09:30AM",
+  "09:30AM-10:00AM",
+  "10:00AM-10:30AM",
+  "10:30AM-11:00AM",
+  "11:00AM-11:30AM",
+  "11:30AM-12:00PM",
+  "12:00PM-12:30PM",
+  "12:30PM-01:00PM",
+  "01:00PM-01:30PM",
+  "01:30PM-02:00PM",
+  "02:00PM-02:30PM",
+  "02:30PM-03:00PM",
+  "03:00PM-03:30PM",
+  "03:30PM-04:00PM",
+  "04:00PM-04:30PM",
+  "04:30PM-05:00PM",
+  "05:00PM-05:30PM",
+  "05:30PM-06:00PM",
+  "06:00PM-06:30PM",
+  "06:30PM-07:00PM",
+  "07:00PM-07:30PM",
+  "07:30PM-08:00PM",
+  "08:00PM-08:30PM",
+  "08:30PM-09:00PM",
+  "09:00PM-09:30PM",
+  "09:30PM-10:00PM",
+  "10:00PM-10:30PM",
+  "10:30PM-11:00PM",
+  "11:00PM-11:30PM",
+  "11:30PM-12:00AM",
 ];
 
 export default function page() {
+  const [loading,setLoading]=useState(false)
+  const [requestedSlots,setRequestedSlots]=useState([])
+  const handleRetrieveSlots = async () => {
+    setLoading(true)
+    const formattedDate = selectedDate.toISOString().split('T')[0];
+    console.log(formattedDate);
+    const url = `http://127.0.0.1:8000/availability-slots/${formattedDate}/`;
+    try {
+      const response = await axios.get(url);
+      setRequestedSlots(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+  // useEffect(async()=> await handleRetrieveSlots(),[])
+
+
+  const hour_minute_converter = (timeslot) => {
+    let firstTime = timeslot.split("-")[0];
+    // Convert the 12-hour time (e.g., "12:30am") to 24-hour format and extract hours and minutes
+    let timeParts = firstTime.match(/(\d+)(:|\s)?(\d*)\s?(am|pm)/i);
+    let hours = parseInt(timeParts[1]);
+    let minutes = parseInt(timeParts[3] || 0);
+    let ampm = timeParts[4].toLowerCase();
+    // Adjust hours for AM/PM
+    if (ampm === "pm" && hours !== 12) {
+      hours += 12; // Convert PM hours to 24-hour format
+    } else if (ampm === "am" && hours === 12) {
+      hours = 0; // Convert 12 AM to 00:00 hours
+    }
+    return [hours,minutes]
+  };
   const [selectedSlots, setSelectedSlots] = useState([]);
-  const [selectedDate,setSelectedDate]=useState(new Date())
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const handleSlotClick = (slot) => {
     setSelectedSlots((prevSelectedSlots) =>
       prevSelectedSlots.includes(slot)
@@ -63,42 +99,87 @@ export default function page() {
         : [...prevSelectedSlots, slot]
     );
   };
-  const handleButtonClick = () => {
-    console.log(selectedSlots);
+  const handleButtonClick = async () => {
+    const slotsData=[]
+    const consultant="1" //todo Replace with actual consultant ID
+    for(let slot of selectedSlots){
+      const hour_minute=hour_minute_converter(slot)
+      let selectedDateCopy = new Date(selectedDate); 
+      selectedDateCopy.setHours(hour_minute[0])
+      selectedDateCopy.setMinutes(hour_minute[1])
+      selectedDateCopy.setSeconds(0)
+      slotsData.push({
+        consultant,
+        start_time: selectedDateCopy,
+        status:"requested"
+      })
+    }
+
+    console.log(slotsData);
+
+    try {
+      const response = await axios.post(" http://127.0.0.1:8000/availability-slots/", { //Todo actual api route
+        slots: slotsData,
+      });
+      setSelectedSlots([])
+      handleRetrieveSlots();
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+
   };
+  useEffect(() => {
+    handleRetrieveSlots();
+  }, [selectedDate]);
+  if(loading){
+    return <div>loading...</div>
+  }
+
+
   return (
     <div className='container mx-auto p-4'>
       <div>
         <DatePicker
           selected={selectedDate}
           onChange={(date) => {
-            setSelectedSlots([])
-            setSelectedDate( date )}}
+            setSelectedSlots([]);
+            setSelectedDate(date);
+          }}
           timeFormat='HH:mm'
           timeIntervals={30}
           timeCaption='time'
           dateFormat='MMMM d, yyyy'
           className='rounded p-2  bg-slate-300 mb-4'
+          minDate={new Date()}
         />
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4'>
         {timeSlots.map((slot, index) => (
-          <div
+          <button
             key={index}
             className={`cursor-pointer border border-gray-200 rounded-lg p-3 shadow-sm hover:shadow-md transition-shadow duration-200  ${
-              selectedSlots.includes(slot) ? "bg-green-300" : "bg-white"
+              requestedSlots.includes(slot) ? "disabled:bg-gray-400 disabled:cursor-not-allowed" : selectedSlots.includes(slot)?"bg-blue-400":"bg-white"
             }`}
+            disabled={requestedSlots.includes(slot)}
             onClick={() => handleSlotClick(slot)}
           >
             {slot}
-          </div>
+          </button>
         ))}
       </div>
       <button
         className='mt-4 bg-blue-500 text-white p-2 rounded'
         onClick={handleButtonClick}
       >
-        Log Selected Slots
+        Submit
+      </button>
+      <button 
+        className="mt-4 bg-green-500 text-white p-2 rounded"
+        onClick={handleRetrieveSlots}
+      >
+        Retrieve Slots
       </button>
     </div>
   );

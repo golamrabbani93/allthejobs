7b708cbbb3ce7a15788fc8913856e14054a24fc4
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-26 18:26:24 +0600

    dashboard top card layout fix

--- components/dashboard-pages/employers-dashboard/dashboard/components/TopCardBlock.jsx
+++ components/dashboard-pages/employers-dashboard/dashboard/components/TopCardBlock.jsx
@@ -33,7 +33,7 @@ const TopCardBlock = () => {
 	return (
 		<>
 			{cardContent.map((item) => (
-				<div className="ui-block col-xl-3 col-lg-6 col-md-6 col-sm-12" key={item.id}>
+				<div className="ui-block col-xl-6 col-lg-6 col-md-6 col-sm-12" key={item.id}>
 					<div className={`ui-item ${item.uiClass}`}>
 						<div className="left">
 							<i className={`icon la ${item.icon}`}></i>

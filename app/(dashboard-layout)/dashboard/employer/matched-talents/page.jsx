b5bb785f1a6f3fcf39fa215312541ac600eeef23
Golam Rabbani <golamrabbani9373@gmail.com>
2025-01-29 17:51:59 +0600

    delete unneccesary route in consultant dashboard

--- app/(dashboard-layout)/dashboard/employer/matched-talents/page.jsx
+++ app/(dashboard-layout)/dashboard/employer/matched-talents/page.jsx
@@ -1,5 +1,9 @@
 import MatchingTalent from '@/components/dashboard-pages/employers-dashboard/matching-talent';
 
+export const metadata = {
+	title: 'Matched Talent || AllTheJobs',
+	description: 'AllTheJobs',
+};
 const page = () => {
 	return (
 		<>

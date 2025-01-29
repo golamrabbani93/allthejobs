1c8b7f07dbb4fc7593753a5cf92daad62b5a2c6f
Golam Rabbani <golamrabbani9373@gmail.com>
2025-01-29 17:52:35 +0600

    delete unneccesary route in consultant dashboard components

--- components/dashboard-pages/consultant-dashboard/meeting-request/index.jsx
+++ components/dashboard-pages/consultant-dashboard/meeting-request/index.jsx
@@ -1,66 +1,59 @@
-"use client";
-import MobileMenu from "../../../header/MobileMenu";
-import LoginPopup from "../../../common/form/login/LoginPopup";
-import DashboardEmployerSidebar from "../../../header/DashboardEmployerSidebar";
-import BreadCrumb from "../../BreadCrumb";
-import TopCardBlock from "./components/TopCardBlock";
-import ProfileChart from "./components/ProfileChart";
-import Notification from "./components/Notification";
-import Applicants from "./components/Applicants";
-import CopyrightFooter from "../../CopyrightFooter";
-import MenuToggler from "../../MenuToggler";
-import DashboardEmployersHeader from "@/components/header/DashboardEmployersHeader";
-import { useSelector } from "react-redux";
-import DashboardConsultantSidebar from "@/components/header/DashboardConsultantSidebar";
-import CallList from "@/app/(others)/video-chat3/Components/CallList";
+'use client';
+import MobileMenu from '../../../header/MobileMenu';
+import LoginPopup from '../../../common/form/login/LoginPopup';
+import BreadCrumb from '../../BreadCrumb';
+import CopyrightFooter from '../../CopyrightFooter';
+import MenuToggler from '../../MenuToggler';
+import DashboardConsultantSidebar from '@/components/header/DashboardConsultantSidebar';
+import CallList from '@/app/(others)/video-chat3/Components/CallList';
+import DashboardConsultantHeader from '@/components/header/DashboardConsultantHeader';
 
 const Index = () => {
-  const user = useSelector((state) => state.user);
-  return (
-    <div className='page-wrapper dashboard'>
-      <span className='header-span'></span>
-      {/* <!-- Header Span for hight --> */}
+	return (
+		<div className="page-wrapper dashboard">
+			<span className="header-span"></span>
+			{/* <!-- Header Span for hight --> */}
 
-      <LoginPopup />
-      {/* End Login Popup Modal */}
+			<LoginPopup />
+			{/* End Login Popup Modal */}
 
-      <DashboardEmployersHeader />
-      {/* End Header */}
+			<DashboardConsultantHeader />
+			{/* End Header */}
 
-      <MobileMenu />
-      {/* End MobileMenu */}
+			<MobileMenu />
+			{/* End MobileMenu */}
 
-      <DashboardConsultantSidebar />
-      {/* <!-- End User Sidebar Menu --> */}
+			<DashboardConsultantSidebar />
+			{/* <!-- End User Sidebar Menu --> */}
 
-      {/* <!-- Dashboard --> */}
-      <section className='user-dashboard'>
-        <div className='dashboard-outer'>
-          <BreadCrumb title={`Incoming Meeting Request`} />
-          {/* breadCrumb */}
+			{/* <!-- Dashboard --> */}
+			<section className="user-dashboard">
+				<div className="dashboard-outer">
+					<BreadCrumb title={`Incoming Meeting Request`} />
+					{/* breadCrumb */}
 
-          <MenuToggler />
-          {/* Collapsible sidebar button */}
+					<MenuToggler />
+					{/* Collapsible sidebar button */}
 
-          <div className='h-screen'>
-            <section className='flex size-full flex-col'>
-              <CallList type='request'></CallList>
-            </section>
-            {/* <div className="row">
+					<div className="h-screen">
+						<section className="flex size-full flex-col">
+							<CallList type="request"></CallList>
+						</section>
+						{/* <div className="row">
 							<TopCardBlock />
 						</div> */}
-          </div>
-          {/* End .row top card block */}
-        </div>
-        {/* End dashboard-outer */}
-      </section>
-      {/* <!-- End Dashboard --> */}
+					</div>
+					{/* End .row top card block */}
+				</div>
+				{/* End dashboard-outer */}
+			</section>
+			{/* <!-- End Dashboard --> */}
 
-      <CopyrightFooter />
-      {/* <!-- End Copyright --> */}
-    </div>
-    // End page-wrapper
-  );
+			<CopyrightFooter />
+			{/* <!-- End Copyright --> */}
+		</div>
+		// End page-wrapper
+	);
 };
 
 export default Index;

fc7fb0bba7234147e90ed3bcb74608ff22dfe96b
golamrabbani93 <golamrabbani9373@gmail.com>
2025-01-29 13:51:17 +0600

    matched talent route and compo crreate complete

---
+++ app/(dashboard-layout)/dashboard/employer/matched-talents/page.jsx
@@ -0,0 +1,11 @@
+import MatchingTalent from '@/components/dashboard-pages/employers-dashboard/matching-talent';
+
+const page = () => {
+	return (
+		<>
+			<MatchingTalent />
+		</>
+	);
+};
+
+export default page;
---
+++ components/dashboard-pages/employers-dashboard/matching-talent/index.jsx
@@ -0,0 +1,72 @@
+'use client';
+
+import MobileMenu from '../../../header/MobileMenu';
+import LoginPopup from '../../../common/form/login/LoginPopup';
+import DashboardEmployerSidebar from '../../../header/DashboardEmployerSidebar';
+import BreadCrumb from '../../BreadCrumb';
+import CopyrightFooter from '../../CopyrightFooter';
+import WidgetContentBox from './components/WidgetContentBox';
+import WidgetTopFilterBox from './components/WidgetTopFilterBox';
+import MenuToggler from '../../MenuToggler';
+import DashboardEmployersHeader from '@/components/header/DashboardEmployersHeader';
+import {useState} from 'react';
+
+const index = () => {
+	const [selectJob, setSelectJob] = useState({});
+	return (
+		<div className="page-wrapper dashboard">
+			<span className="header-span"></span>
+			{/* <!-- Header Span for hight --> */}
+
+			<LoginPopup />
+			{/* End Login Popup Modal */}
+
+			<DashboardEmployersHeader />
+			{/* End Header */}
+
+			<MobileMenu />
+			{/* End MobileMenu */}
+
+			<DashboardEmployerSidebar />
+			{/* <!-- End User Sidebar Menu --> */}
+
+			{/* <!-- Dashboard --> */}
+			<section className="user-dashboard">
+				<div className="dashboard-outer">
+					<BreadCrumb title="Matched Talents!" />
+					{/* breadCrumb */}
+
+					<MenuToggler />
+					{/* Collapsible sidebar button */}
+
+					<div className="row">
+						<div className="col-lg-12">
+							{/* <!-- Ls widget --> */}
+							<div className="ls-widget">
+								<div className="tabs-box">
+									<div className="widget-title">
+										<h4>All Applicant List</h4>
+										<WidgetTopFilterBox setSelectJob={setSelectJob} />
+									</div>
+									{/* End top widget filter bar */}
+
+									<WidgetContentBox selectJob={selectJob} />
+									{/* End widget-content */}
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* End .row */}
+				</div>
+				{/* End dashboard-outer */}
+			</section>
+			{/* <!-- End Dashboard --> */}
+
+			<CopyrightFooter />
+			{/* <!-- End Copyright --> */}
+		</div>
+		// End page-wrapper
+	);
+};
+
+export default index;

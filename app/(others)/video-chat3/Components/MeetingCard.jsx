46b2b6af4ca2d31a1f24c168dc25e76a218c80bd
Hamim <shoeb.hamim@gmail.com>
2025-01-29 23:35:01 +0600

    participant name for consultant view added

--- app/(others)/video-chat3/Components/MeetingCard.jsx
+++ app/(others)/video-chat3/Components/MeetingCard.jsx
@@ -30,8 +30,8 @@ const MeetingCard = ({
 }) => {
 
   const { toast } = useToast();
-  const baseUrl=process.env.NEXT_PUBLIC_BASE_URL
-  console.log('this is base url',baseUrl);
+  console.log(customData);
+
 
   return (
     <section className="flex min-h-[200px] w-full flex-col gap-3 justify-between rounded-[14px]  px-5 py-8 xl:max-w-[568px] border drop-shadow-lg bg-white">
@@ -72,13 +72,18 @@ const MeetingCard = ({
             </Button> */}
           </div>
         )}
-{       role==="talent"&& <div className="flex justify-center items-center">
+{role==="talent"&& <div className="flex justify-center items-center">
           <svg className="w-6 mx-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="10" cy="6" r="4" stroke="#1C274C" stroke-width="1.5"></circle> <path d="M19 2C19 2 21 3.2 21 6C21 8.8 19 10 19 10" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M17 4C17 4 18 4.6 18 6C18 7.4 17 8 17 8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M17.9975 18C18 17.8358 18 17.669 18 17.5C18 15.0147 14.4183 13 10 13C5.58172 13 2 15.0147 2 17.5C2 19.9853 2 22 10 22C12.231 22 13.8398 21.8433 15 21.5634" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
           <Link href={`/consultants/${customData.consultant_real_id}`} target="_blank" rel="noopener noreferrer">
             <span className="font-bold cursor-pointer">
               {customData.consultant_name}</span>
           </Link>
           
+        </div>}
+{role==="consultant"&& <div className="flex justify-center items-center">
+          <svg className="w-6 mx-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <circle cx="10" cy="6" r="4" stroke="#1C274C" stroke-width="1.5"></circle> <path d="M19 2C19 2 21 3.2 21 6C21 8.8 19 10 19 10" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M17 4C17 4 18 4.6 18 6C18 7.4 17 8 17 8" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> <path d="M17.9975 18C18 17.8358 18 17.669 18 17.5C18 15.0147 14.4183 13 10 13C5.58172 13 2 15.0147 2 17.5C2 19.9853 2 22 10 22C12.231 22 13.8398 21.8433 15 21.5634" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"></path> </g></svg>
+            <span className="font-bold">
+              {customData.talent_name}</span>
         </div>}
       </article>
     </section>

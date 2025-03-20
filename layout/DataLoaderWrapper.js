56cfb4432fc3a1e89c3497ac7a6398f817954476
Golam Rabbani <golamrabbani9373@gmail.com>
2025-03-20 16:52:15 +0600

    change data loader method

--- layout/DataLoaderWrapper.js
+++ layout/DataLoaderWrapper.js
@@ -17,7 +17,7 @@ import {
 	fetchTalentData,
 	fetchUserInformation,
 } from '@/services/GenerateAllData';
-import {useEffect, useState} from 'react';
+import {use, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 
 const DataLoaderWrapper = ({children}) => {
@@ -27,8 +27,27 @@ const DataLoaderWrapper = ({children}) => {
 	const fetchAllData = async () => {
 		try {
 			const data = await fetchData();
-			const userData = await fetchUserInformation(user?.email);
 			const packages = await fetchPackages();
+			if (packages?.length > 0) {
+				const talentPackages = packages?.filter((item) => item.target_role === 'talent');
+				dispatch(setTalentPackages(talentPackages));
+				const employerPackages = packages?.filter((item) => item.target_role === 'employer');
+				dispatch(setEmployerPackages(employerPackages));
+			}
+			dispatch(setTalentsData(data.talents));
+			dispatch(setJobsData(data.jobs));
+			dispatch(setConsultantsData(data.consultants));
+			dispatch(setGlobalDataLoading(false));
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		} finally {
+			dispatch(setGlobalDataLoading(false));
+		}
+	};
+
+	const fetchUserData = async () => {
+		try {
+			const userData = await fetchUserInformation(user?.email);
 			if (userData?.role === 'talent') {
 				const talentData = await fetchTalentData(userData.user_id);
 				dispatch(setUserRoleBasedData(talentData));
@@ -41,36 +60,31 @@ const DataLoaderWrapper = ({children}) => {
 				const employerData = await fetchEmployerData(userData.user_id);
 				dispatch(setUserRoleBasedData(employerData));
 			}
-			if (packages?.length > 0) {
-				const talentPackages = packages?.filter((item) => item.target_role === 'talent');
-				dispatch(setTalentPackages(talentPackages));
-				const employerPackages = packages?.filter((item) => item.target_role === 'employer');
-				dispatch(setEmployerPackages(employerPackages));
-			}
+
 			if (userData?.user_id === user?.user_id) {
 				dispatch(setUserData(userData));
 			}
-			dispatch(setTalentsData(data.talents));
-			dispatch(setJobsData(data.jobs));
-			dispatch(setConsultantsData(data.consultants));
-
-			dispatch(setGlobalDataLoading(false));
+			dispatch(setUserData(userData));
 		} catch (error) {
-			console.error('Error fetching data:', error);
-		} finally {
-			dispatch(setGlobalDataLoading(false));
+			console.error('Error fetching user data:', error);
 		}
 	};
+
 	useEffect(() => {
-		// fetch all data and revalidate every 8 minutes
+		if (user?.user_id) {
+			fetchUserData();
+		}
+	}, [user?.user_id]);
+	useEffect(() => {
 		dispatch(setGlobalDataLoading(true));
 		fetchAllData();
+		// fetch all data and revalidate every 8 minutes
 		const interval = setInterval(() => {
 			dispatch(setGlobalDataLoading(false));
 			fetchAllData();
-		}, 48000);
+		}, 8 * 60 * 1000);
 		return () => clearInterval(interval);
-	}, [user]);
+	}, []);
 	return <>{children}</>;
 };
 
